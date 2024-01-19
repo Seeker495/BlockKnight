64fb291e1a8b2816dce60aaaabb5a50bb45018a5
yksA_Ascii <asakurainu8@gmail.com>
2024-01-19 13:08:06 +0900

    モック作成

---
+++ Assets/Libraries/AsciiUtil/UI/MaskSlider/MaskSliderView.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+/// <summary>
+/// マスクを使ったスライダー
+/// </summary>
+public class MaskSliderView : MonoBehaviour
+{
+    [SerializeField]
+    private Image fillImage;
+    [SerializeField]
+    private Slider slider;
+    private float maxValue = 1.0f;
+
+    private void Start()
+    {
+        slider.fillRect = null;
+        slider.onValueChanged.AddListener(SetValue);
+    }
+
+    // スライダーの値を設定するメソッド
+    private void SetValue(float value)
+    {
+        fillImage.fillAmount = value / maxValue;
+    }
+}
