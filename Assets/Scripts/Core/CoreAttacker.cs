5a1181683757d2f8131d3fa0109a86cb2fc2b2ed
yksA_Ascii <asakurainu8@gmail.com>
2024-01-22 15:33:29 +0900

    ためしにブロックヒット時のVFX追加

--- Assets/Scripts/Core/CoreAttacker.cs
+++ Assets/Scripts/Core/CoreAttacker.cs
@@ -1,13 +1,17 @@
+using AsciiUtil;
 using UnityEngine;
 
 public class CoreAttacker : MonoBehaviour
 {
     [SerializeField]
     private float damage;
+    [SerializeField]
+    private ParticlePlayer particlePlayer;
     private void OnCollisionEnter2D(Collision2D other)
     {
         IDamageable damageable = null;
         if (!other.gameObject.TryGetComponent(out damageable)) return;
+        particlePlayer.Play("HitBlock", other.transform.position);
         damageable.TakeDamage(damage);
     }
 }
