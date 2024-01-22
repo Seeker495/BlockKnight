a89151c5ffef0fdf4f804740ce9ede4150ac291a
yksA_Ascii <asakurainu8@gmail.com>
2024-01-22 15:05:19 +0900

    コアの攻撃判定実装

--- Assets/Scripts/Core/CoreAttacker.cs
+++ Assets/Scripts/Core/CoreAttacker.cs
@@ -1,18 +1,13 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class CoreAttacker : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField]
+    private float damage;
+    private void OnCollisionEnter2D(Collision2D other)
     {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        IDamageable damageable = null;
+        if (!other.gameObject.TryGetComponent(out damageable)) return;
+        damageable.TakeDamage(damage);
     }
 }
