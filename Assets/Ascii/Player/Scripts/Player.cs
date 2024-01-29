4d5d4cd041e165182d9eabb3ebb3a5f5ffb656c0
yksA_Ascii <asakurainu8@gmail.com>
2024-01-29 15:45:08 +0900

    攻撃処理,アニメーションの実装

--- Assets/Ascii/Player/Scripts/Player.cs
+++ Assets/Ascii/Player/Scripts/Player.cs
@@ -8,7 +8,11 @@ public class Player : MonoBehaviour
     private PlayerMover playerMover;
     [SerializeField]
     private PlayerJumper playerJumper;
-
+    [SerializeField]
+    private PlayerAttacker playerAttacker;
+    [SerializeField]
+    private AttackAreaController attackAreaController;
+    public AttackAreaController AttackAreaController => attackAreaController;
     private PlayerInfo playerInfo;
     public PlayerInfo PlayerInfo => playerInfo;
     private GameController gameController;
@@ -17,16 +21,21 @@ public class Player : MonoBehaviour
     public Rigidbody2D RigidBody => rigidBody;
     private PlayerAnimationController animationController;
     public PlayerAnimationController AnimationController => animationController;
-
+    private StaminaPresenter staminaPresenter;
+    public StaminaPresenter StaminaPresenter => staminaPresenter;
     private void Start()
     {
         rigidBody = GetComponent<Rigidbody2D>();
         animationController = GetComponent<PlayerAnimationController>();
+        staminaPresenter = GetComponent<StaminaPresenter>();
         playerInfo = InfomationProvider.Instance.PlayerInfo;
         gameController = new GameController();
         gameController.Enable();
 
+        staminaPresenter.Initialize(playerInfo.MaxStamina);
+        attackAreaController.Initialize(playerInfo);
         playerMover.Initialize(this);
         playerJumper.Initialize(this);
+        playerAttacker.Initialize(this);
     }
 }
--- Assets/Ascii/Player/Scripts/PlayerAnimationController.cs
+++ Assets/Ascii/Player/Scripts/PlayerAnimationController.cs
@@ -4,38 +4,36 @@ using UnityEngine;
 public class PlayerAnimationController : MonoBehaviour
 {
     private Animator animator;
-    private FloatReactiveProperty horizontalVelocity = new FloatReactiveProperty(0f);
-    private BoolReactiveProperty isJumping = new BoolReactiveProperty(false);
-    private BoolReactiveProperty isGrounded = new BoolReactiveProperty(false);
+
     void Start()
     {
         animator = GetComponent<Animator>();
-
-
-        horizontalVelocity.Subscribe(value => animator.SetFloat("HorizontalVelocity", value)).AddTo(this);
     }
 
     public void SetHorizontalVelocity(Vector2 value)
     {
-        horizontalVelocity.Value = Mathf.Abs(value.x);
+        animator.SetFloat("HorizontalVelocity", Mathf.Abs(value.x));
         CheckAndChangeDirection(value.normalized);
+    }
+
+    public void SetAttacking(bool value)
+    {
+        animator.SetBool("IsAttacking", value);
+    }
+
+    public void SetIsJumping(bool value)
+    {
+        animator.SetBool("IsJumping", value);
+    }
+
+    public void SetIsGrounded(bool value)
+    {
+        animator.SetBool("IsGrounded", value);
     }
 
     private void CheckAndChangeDirection(Vector2 value)
     {
         if (value.x == 0f) return;
         transform.localScale = new Vector3(value.x, 1f, 1f);
-    }
-
-    public void SetIsJumping(bool value)
-    {
-        isJumping.Value = value;
-        animator.SetBool("IsJumping", value);
-    }
-
-    public void SetIsGrounded(bool value)
-    {
-        isGrounded.Value = value;
-        animator.SetBool("IsGrounded", value);
     }
 }
