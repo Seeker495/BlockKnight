6b24e584e85a7766f04e3e480452db7ce4f524a8
yksA_Ascii <asakurainu8@gmail.com>
2024-01-29 10:45:22 +0900

    敵の見た目通りにブロックを配置するヘルパーの実装

---
+++ Assets/Ascii/Block/SpriteSplitter.cs
@@ -0,0 +1,11 @@
+using UnityEngine;
+
+public class SpriteSplitter : MonoBehaviour
+{
+    [SerializeField]
+    public Sprite sourceSprite;
+    [SerializeField]
+    public int textureSizeX, textureSizeY;
+    [SerializeField]
+    public GameObject blockPrefab;
+}
