04a628550b152f9601e97288db3b70bbc2e12305
yksA_Ascii <asakurainu8@gmail.com>
2024-02-18 18:05:07 +0900

    ツイートインフォ追加

--- Assets/Ascii/TweetInfo.cs
+++ Assets/Ascii/TweetInfo.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[CreateAssetMenu(fileName = "TweetInfo", menuName = "Ascii/TweetInfo", order = 1)]
 public class TweetInfo : ScriptableObject
 {
     [SerializeField, Header("UnityRoomのゲームID")]
