a234d6ae4ec76c3b7b2ec0f75e545054adb311cd
yksA_Ascii <asakurainu8@gmail.com>
2024-02-18 18:07:43 +0900

    ヒットストップをスローモーションに変更

--- Assets/Ascii/HitStopManager.cs
+++ Assets/Ascii/HitStopManager.cs
@@ -3,9 +3,11 @@ using UnityEngine;
 
 public class HitStopManager
 {
+    [SerializeField]
+    private static float onHitTimeScale = 0.1f;
     public static async void HitStop(float stopSec)
     {
-        Time.timeScale = 0;
+        Time.timeScale = onHitTimeScale;
         await UniTask.WaitForSeconds(stopSec, ignoreTimeScale: true);
         Time.timeScale = 1;
     }
